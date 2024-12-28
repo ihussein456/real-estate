1d568cc934c982ed87b3562c5a4ca6d498b57b52
Ibrahim <ihussein456@gmail.com>
2024-12-28 15:08:13 +0000

    Added assests

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,9 +9,9 @@ export default function Index () {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff',
       }}
     >
+      <Text className='font-bold text-lg my-10'>Welcome to ReState</Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
